538ae1c57d3090d48b78abfcbd423759d689c99d
Daniel Shapiro <d.shapiro@berkeley.edu>
2019-04-09 02:19:29 -0700

    add graphviz link to program output

--- src/main/scala/cladograms/Main.scala
+++ src/main/scala/cladograms/Main.scala
@@ -1,5 +1,8 @@
 package cladograms
 
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
 import org.jsoup.Jsoup
 
 /**
@@ -11,9 +14,14 @@ object Main extends App {
     new EnWikipediaClade(name, Some("/wiki/" + name.replaceAll(" ", "_")))
   }
 
+  def dotToGraphVizUrl(dot: String): String = {
+    "https://dreampuf.github.io/GraphvizOnline/#" +
+      URLEncoder.encode(dot, StandardCharsets.UTF_8.toString).replaceAll("\\+", " ")
+  }
+
   val leaves = (args map getLeafClade).toList
   val cladSet = Cladogram.construct(leaves)
   val dots = cladSet map (cladogram => cladogram.toDOT(cladogram.clade.name))
 
-  dots.foreach (dot => println(dot + "\n"))
+  dots.foreach (dot => println(dot + "\n" + dotToGraphVizUrl(dot) + "\n"))
 }
